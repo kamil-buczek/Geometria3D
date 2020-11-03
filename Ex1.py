b73e186091c19dfb7a34d7787546fed64d4a38bd
Kamil Buczek <kamil.andrzej.buczek@gmail.com>
2020-11-03 16:51:13 +0100

    Initial commit

---
+++ Ex1.py
@@ -0,0 +1,56 @@
+import numpy as np
+import math
+
+vec1 = np.array([3, 4, 0])
+vec2 = np.array([8, 4, 0])
+
+
+def suma_wektorow(vec1, vec2):
+    return np.add(vec1, vec2)
+
+
+def iloczyn_skalarny(vec1, vec2):
+    return np.dot(vec1, vec2)
+
+
+def iloczyn_wektorowy(vec1, vec2):
+    return np.cross(vec1, vec2)
+
+
+def cos_alfa(vec1, vec2):
+    licznik = iloczyn_skalarny(vec1, vec2)
+    mianownik = iloczyn_skalarny(norma_wektora(vec1), norma_wektora(vec2))
+    wynik = licznik/mianownik
+    return wynik
+
+
+def norma_wektora(vec):
+    return np.sqrt(np.dot(vec, vec))
+
+
+def kat_pomiedzy_wektorami(vec1, vec2):
+    cos = cos_alfa(vec1, vec2)
+    katrad = math.acos(cos)
+    return math.degrees(katrad)
+
+
+def projekcja_wektora(vec1, vec2):
+    # Projekcja wektora vec1 na wektor vec2
+    licznik = iloczyn_skalarny(vec1, vec2)
+    mianownik = norma_wektora(vec2)*norma_wektora(vec2)
+    wynik = np.multiply((licznik/mianownik), vec2)
+    return wynik
+
+
+def normalizacja_wektora(vec):
+    return vec/norma_wektora(vec)
+
+
+print(suma_wektorow(vec1, vec2))
+print(iloczyn_skalarny(vec1, vec2))
+print(iloczyn_wektorowy(vec1, vec2))
+print(norma_wektora(vec1))
+print(cos_alfa(vec1, vec2))
+print(kat_pomiedzy_wektorami(vec1, vec2))
+print(projekcja_wektora(vec1, vec2))
+print(normalizacja_wektora(vec1))
